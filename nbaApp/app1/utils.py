2f77691868e4368e87f292274fd169e69b192bb4
Salman Syed <salman_syed55@hotmail.com>
2015-10-11 00:42:06 -0400

    token setup

--- nbaApp/app1/utils.py
+++ nbaApp/app1/utils.py
@@ -1,4 +1,15 @@
+from rest_framework.views import exception_handler
 import logging
+
+def api_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    if response is not None:
+    	resp_data = {}
+    	resp_data['status_code'] = response.status_code
+    	resp_data['message'] = response.data['detail']
+    	del response.data['detail']
+        response.data['error'] = resp_data
+    return response
 
 class LogHandler(object):
 	LOG_LEVEL = logging.DEBUG
@@ -11,5 +22,5 @@ class LogHandler(object):
 		formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(filename)s: %(message)s')
 		file_handle.setFormatter(formatter)
 		self.logger.addHandler(file_handle)
-	def log(self, severity, msg):
+	def log(self, severity, msg): 
 		self.logger.log(severity, msg)
